8ec051af9c05ba5856034098255a27302cc22493
Khadijah Shabir <133489629+khadijah-Shabir@users.noreply.github.com>
2024-05-29 23:23:10 +0500

    Time: 107 ms (73.66%) | Memory: 19.1 MB (71.05%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,8 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        count = {}
         
-        unique = [num for num in nums if nums.count(num) == 1]
-
-        minimum = min(unique)
-
-        return minimum
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+        
+        unique = [num for num in count if count[num] == 1]
+        
+        return min(unique)
