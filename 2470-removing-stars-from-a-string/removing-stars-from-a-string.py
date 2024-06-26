bebc4480a5f459ced319412b9569a1f1f5b9313a
Khadijah Shabir <133489629+khadijah-Shabir@users.noreply.github.com>
2024-06-26 06:41:32 +0500

    Time: 136 ms (61.28%) | Memory: 18.2 MB (47.01%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack=[]
+        for letter in s: 
+            if letter=='*':
+                stack.pop()
+            else:
+                stack.append(letter)
+        return "".join(stack)
+       
+        
