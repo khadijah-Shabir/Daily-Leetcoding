50b250e77f56da3f08d96b10eee19d1cfb455f21
Khadijah Shabir <133489629+khadijah-Shabir@users.noreply.github.com>
2024-06-28 15:24:56 +0500

    Time: 52 ms (58.54%) | Memory: 16.6 MB (18.10%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.py
@@ -1,12 +1,13 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        subarray=[]
+        subarr=[]
         ans=0
         n=len(arr)
         for i in range(n):
             for j in range(i+1, n+1):
-                if(j-i)%2:
-                    ans+=sum(arr[i:j])
+                if (j-i)%2:
+                    ans+= sum(arr[i:j])
         return ans
-    
+
+       
         
