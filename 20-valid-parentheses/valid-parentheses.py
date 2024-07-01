8aacc9c9fcd58d87211ecaaee5db145652a72ce5
Khadijah Shabir <133489629+khadijah-Shabir@users.noreply.github.com>
2024-07-01 10:59:10 +0500

    Time: 41 ms (19.89%) | Memory: 16.5 MB (48.23%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,22 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        bracket_dict = {
+        dict = {
             ")": "(",
             "]": "[",
             "}": "{"
         }
-
         stack = []
-        for i in s:
-            if i in bracket_dict.values():  # Check if i is an opening bracket
-                stack.append(i)  # Push opening bracket onto the stack
-            elif i in bracket_dict.keys():  # Check if i is a closing bracket
-                if not stack or stack.pop() != bracket_dict[i]:
+        for paren in s:
+            if paren in dict:
+                if stack and stack[-1]==dict[paren]:
+                    stack.pop()
+                else:
                     return False
             else:
-                return False  # If i is neither opening nor closing bracket, return False
-        
-        return not stack  # Return True if stack is empty, False otherwise
+                stack.append(paren)
+        return not stack
 
 
             
