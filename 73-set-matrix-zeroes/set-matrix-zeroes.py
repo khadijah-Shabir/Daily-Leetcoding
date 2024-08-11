774dc9704b7abaa9d6f16ce121b7304807a50e83
Khadijah Shabir <133489629+khadijah-Shabir@users.noreply.github.com>
2024-08-11 07:30:58 +0500

    Time: 98 ms (81.64%) | Memory: 17.4 MB (84.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,16 +3,30 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        arr=[]
-        brr=[]
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j]==0:
-                    arr.append(i)
-                    brr.append(j)
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in arr or j in brr:
-                    matrix[i][j]=0
-        return matrix
+        Rows, Cols= len(matrix), len(matrix[0])
+        rowZero=False
+
+        for r in range(Rows):
+            for c in range(Cols):
+                if matrix[r][c]==0:
+                    matrix[0][c]=0
+
+                    if r>0: 
+                        matrix[r][0]=0
+                    else:
+                        rowZero=True
+        
+        for r in range(1, Rows):
+            for c in range(1, Cols):
+                if matrix[0][c]==0 or matrix[r][0]==0:
+                    matrix[r][c]=0
+
+        if matrix[0][0]==0:
+            for r in range(Rows):
+                matrix[r][0]=0
+        
+        if rowZero:
+            for c in range(Cols):
+                matrix[0][c]=0
+
                 
