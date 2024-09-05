66b08284916d08ac9a18097b116992accb5fb3a1
Khadijah Shabir <133489629+khadijah-Shabir@users.noreply.github.com>
2024-09-06 00:32:43 +0500

    Time: 133 ms (28.29%) | Memory: 66.8 MB (23.64%) - LeetSync

--- 2860-sort-by/sort-by.js
+++ 2860-sort-by/sort-by.js
@@ -5,5 +5,8 @@
  */
 var sortBy = function(arr, fn) {
     return arr.sort((a,b)=>fn(a)-fn(b))
+    //we are using compare function inside sort function which is optional and which will automatically sort the two values
+
+    //for descending order we can use fn(b)-fn(a)
     
 };
